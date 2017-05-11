2c338f02884183182c06fa4d510936c26a143900
Semih Bozdemir <semih.bozdemir@monitise.com>
2017-05-11 16:57:07 +0300

    Add firebase sdk

--- index.js
+++ index.js
@@ -2,10 +2,26 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const firebase = require('firebase');
 
 const restService = express();
 restService.use(bodyParser.json());
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyAp_AD1BbSpYzXON5m2HEuymRdpppuVRDY",
+    authDomain: "hackathon-qkquyv.firebaseapp.com",
+    databaseURL: "https://hackathon-qkquyv.firebaseio.com",
+    projectId: "hackathon-qkquyv",
+    storageBucket: "hackathon-qkquyv.appspot.com",
+    messagingSenderId: "196891096619"
+};
+firebase.initializeApp(config);
+firebase.database().ref('accounts/john').set({
+    name: "John"
+});
+
+
 
 restService.post('/hook', function (req, res) {
 
@@ -18,6 +34,13 @@ restService.post('/hook', function (req, res) {
             var requestBody = req.body;
 
             if (requestBody.result) {
+                if (requestBody.result.parameters) {
+                    var parameters = requestBody.result.parameters;
+
+                    // todo doTransaction(parameters.account, parameters.money);
+                }
+
+                // remove these lines
                 speech = '';
 
                 if (requestBody.result.fulfillment) {
@@ -28,11 +51,13 @@ restService.post('/hook', function (req, res) {
                 if (requestBody.result.action) {
                     speech += 'action: ' + requestBody.result.action;
                 }
+                // end: remove these lines
             }
         }
 
         console.log('result: ', speech);
 
+        // todo return result inside fulfillment node
         return res.json({
             speech: speech,
             displayText: speech,
